d6e5dd04024f616432e1a8e5006fb101e9d135a7
gvarys <johnnyzaev@gmail.com>
2023-02-13 13:54:56 +0300

    player can move now

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] private float speed = 10f;
     private void Update()
     {
-        transform.Translate(0.01f, 0, 0);
+        var xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var zValue = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(xValue, 0, zValue);
     }
 }
