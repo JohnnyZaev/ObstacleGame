8bd98dfaf5401010c0a36d60014c9796df2056fd
gvarys <johnnyzaev@gmail.com>
2023-02-13 14:31:46 +0300

    some cleaning in mover scripts

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -5,6 +5,11 @@ public class Mover : MonoBehaviour
     [SerializeField] private float speed = 10f;
     private void Update()
     {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
         var xValue = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
         var zValue = Input.GetAxis("Vertical") * Time.deltaTime * speed;
         transform.Translate(xValue, 0, zValue);
